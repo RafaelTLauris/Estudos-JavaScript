d712a957b3e760c05bb07e996ac21afe8559704c
RafaelTLauris <81991937+RafaelTLauris@users.noreply.github.com>
2021-06-08 14:29:06 -0300

    Aula7e8
    Operadores

---
+++ Aula_08/ex001.js
@@ -0,0 +1,57 @@
+//OPERADORES ARITMÉTICOS             ** TESTAR NO NODE.JS **
+
+var soma = 5 + 5
+soma
+
+var subtracao = 10 - 5
+subtracao
+
+var multiplicacao = 2 * 6
+multiplicacao
+
+var divisao = 10 / 2
+divisao
+
+var RestoDaDivisaoInteira = 5 % 3          // É o resto da (oq sobra) da divisão
+RestoDaDivisaoInteira
+
+var potencia = 5 ** 2
+potencia
+
+
+
+
+
+//OPERADORES RELACIONAIS, LÓGICOS E TERNÁRIOS             ** TESTAR NO NODE.JS **
+
+
+var maior = 5 > 2
+maior
+
+var menor = 2 < 5
+menor
+
+var maiorIgual = 5 >= 5
+maiorIgual
+
+var menorIgual = 2 <= 2
+menorIgual
+
+var igual = 10 == 10
+igual
+
+var diferente = 10 != 11
+diferente
+
+
+
+//IDENTIDADE
+
+var x = 5 == 5
+var x = 5 == '5'
+var x = 5 === '5'
+var x = 5 === 5
+
+
+
+
