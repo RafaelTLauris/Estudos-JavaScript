9e63896040036ed29306394d6b61cdc20c27965c
RafaelTLauris <81991937+RafaelTLauris@users.noreply.github.com>
2021-06-14 13:49:29 -0300

    Aula 11 e 12

---
+++ Aula_11/ex009.js
@@ -0,0 +1,12 @@
+//CONDIÇÃO COMPOSTA
+
+var pais = 'EUA'
+
+console.log(`Vivendo em ${pais}`)
+
+if(pais == "Brasil"){
+    console.log('Brasilerio')
+}
+else{
+    console.log('Você é Estrangeiro')
+}
